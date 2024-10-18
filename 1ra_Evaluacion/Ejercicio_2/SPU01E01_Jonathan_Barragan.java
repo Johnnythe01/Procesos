383c74fad56d1ff928e75db7ced87772215d6e8b
Jonathan <jbarragandela@nigul.cide.es>
2024-10-18 20:14:06 +0200

    gg

---
+++ 1ra_Evaluacion/Ejercicio_2/SPU01E01_Jonathan_Barragan.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+public class SPU01E01_Jonathan_Barragan {
+
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.err.println("Se necesita un programa a ejecutar");
+            System.exit(-1);
+        }
+
+        // Crear el proceso con los argumentos proporcionados
+        ProcessBuilder pb = new ProcessBuilder(args);
+        try {
+            // Iniciar el proceso hijo
+            Process process = pb.start();
+
+            // El padre espera a que el proceso hijo termine
+            int retorno = process.waitFor();
+            
+            // Informar si el proceso se ejecutó correctamente o no
+            System.out.println("La ejecución de " + Arrays.toString(args) + " retorna " + retorno);
+            
+        } catch (IOException ex) {
+            System.err.println("Error de Entrada/Salida durante la ejecución del comando.");
+            System.exit(-1);
+        } catch (InterruptedException ex) {
+            System.err.println("El proceso hijo fue interrumpido inesperadamente.");
+            System.exit(-1);
+        }
+    }
+}
+
